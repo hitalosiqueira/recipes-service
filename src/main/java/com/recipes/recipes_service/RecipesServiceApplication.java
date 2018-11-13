525ed2e6d25df1e58af9878635f6a9f636ff2396
Hitalo Siqueira <hsiqueira@hitalo-pc.localdomain>
2018-11-13 21:31:53 -0200

    first commit: spring initalizr acrhtype

---
+++ src/main/java/com/recipes/recipes_service/RecipesServiceApplication.java
@@ -0,0 +1,12 @@
+package com.recipes.recipes_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipesServiceApplication.class, args);
+    }
+}
